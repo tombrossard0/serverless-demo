904c6a2ed8c71388f5c2742c064edc8934e0ac1e
Tom Brossard <tombrossard0@gmail.com>
2025-09-16 10:14:50 +0200

    feat(terraform)

---
+++ terraform/main.tf
@@ -0,0 +1,155 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  token                       = ""
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    lambda     = "http://ip10-0-40-4-d34gve7tq0k1c7cormtg-4566.direct.lab-boris.fr"
+    apigateway = "http://ip10-0-40-4-d34gve7tq0k1c7cormtg-4566.direct.lab-boris.fr"
+    iam        = "http://ip10-0-40-4-d34gve7tq0k1c7cormtg-4566.direct.lab-boris.fr"
+    dynamodb   = "http://ip10-0-40-4-d34gve7tq0k1c7cormtg-4566.direct.lab-boris.fr"
+  }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "api" {
+  function_name = "hello-api"
+  handler       = "handler.handler"
+  runtime       = "python3.9"
+  role          = aws_iam_role.lambda_exec.arn
+  filename      = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+  timeout       = 15
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.contacts.name
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "contacts" {
+  name         = "contacts"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "hello-api"
+  description = "API REST simulée"
+}
+
+resource "aws_api_gateway_resource" "hello" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_resource" "contact" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "contact"
+}
+
+resource "aws_api_gateway_method" "hello" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.hello.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "contact" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.contact.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "hello" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.hello.id
+  http_method             = aws_api_gateway_method.hello.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "contact" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.contact.id
+  http_method             = aws_api_gateway_method.contact.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.hello.id,
+      aws_api_gateway_resource.contact.id,
+      aws_api_gateway_method.hello.id,
+      aws_api_gateway_method.contact.id,
+      aws_api_gateway_integration.hello.id,
+      aws_api_gateway_integration.contact.id
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.hello,
+    aws_api_gateway_integration.contact
+  ]
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "dev"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lambda_permission" "allow_apigw_hello" {
+  statement_id  = "AllowExecutionFromAPIGatewayHello"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/hello"
+}
+
+resource "aws_lambda_permission" "allow_apigw_contact" {
+  statement_id  = "AllowExecutionFromAPIGatewayContact"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/contact"
+}
---
+++ terraform/output.tf
@@ -0,0 +1,7 @@
+output "api_id" {
+  value = aws_api_gateway_rest_api.api.id
+}
+
+output "lambda_env_table_name" {
+  value = aws_dynamodb_table.contacts.name
+}
